396855c7b32a851bbb3b47530a5b2a9ca56b419c
farvardin <eforgeot@yahoo.fr>
2017-08-07 00:17:58 +0200

    updated gerber

--- pcb/prepare_gerber.sh
+++ pcb/prepare_gerber.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-for i in garvuino_gerber/*.gm1 ; do mv $i $i.gml ; done
+
+for i in garvuino_gerber/*.gm1 ; do cp $i ${i%%.*}.gml ; done
 	
 for j in garvuino_gerber/*_pnp.txt ; do mv $j $j.old ; done
 	
+rm -fr garvuino_gerber_v9.zip
 zip -r garvuino_gerber_v9.zip garvuino_gerber	
 
 	

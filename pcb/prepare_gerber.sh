f1906ebd54010685f69f1fcdf2dd19448f400668
farvardin <eforgeot@yahoo.fr>
2018-01-21 18:42:34 +0100

    new PCB version, 2.10p release. Changelog: - Audio output shifted left so it will fit better on the board (thank you Yoruk!) - Capacitors moved so they will be less close to the switch button - tx pin slightly moved up so the jumper will be less close to the arduino board (it's still adviced to put it below the board) - removed ay/arduino jumper (use the AY>L and AY>R instead) - removed the J6 jumper (not useful) - removed the SD card (use the microSD instead) - added 2 switch buttons - added 2 potentiometers - added room for 1 extra potentiometer instead of the midi port (if needed)

--- pcb/prepare_gerber.sh
+++ pcb/prepare_gerber.sh
@@ -5,7 +5,7 @@ for i in garvuino_gerber/*.gm1 ; do cp $i ${i%%.*}.gml ; done
 	
 for j in garvuino_gerber/*_pnp.txt ; do mv $j $j.old ; done
 	
-rm -fr garvuino_gerber_v9.zip
-zip -r garvuino_gerber_v9.zip garvuino_gerber	
+rm -fr garvuino_gerber_v2-10.zip
+zip -r garvuino_gerber_v2-10.zip garvuino_gerber	
 
 	
